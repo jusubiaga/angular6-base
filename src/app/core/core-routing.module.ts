8aaef774bee685900e4eada0d700960cc565e9aa
Juan Usubiaga <jusubiaga@gmail.com>
2018-09-03 10:12:02 -0300

    fix: clean code; circular dependencies

--- src/app/core/core-routing.module.ts
+++ src/app/core/core-routing.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 import { LoginComponent } from './login/login.component';
-import { AuthGuardService } from '@app/core';
+import { AuthGuardService } from './services/auth-gard.service';
 
 const routes: Routes = [
     {
